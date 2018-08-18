2168d4dcc683f6d26b522bdf94727043fd634c10
Ricky Bruno <ricardobruno_89@hotmail.com>
2018-08-18 16:12:13 -0300

    FIN

--- routes/usuario.js
+++ routes/usuario.js
@@ -4,9 +4,12 @@ var router = express.Router();
 var auth = require('../auth/metodo1/authC');
 var usuario = require('../controllers/usuario')
 
-router.get('/', auth.ensureAuthenticated, usuario.listar); // get sin params es listar todos
-router.get('/:idUsuario', auth.ensureAuthenticated, usuario.dame); // get con params es mostrar 1 usuario
-router.put('/', auth.ensureAuthenticated, usuario.modificar); //put es actualizar
+router.get('/', usuario.listar); // get sin params es listar todos
+// las rutas que esten abajo de este router.use auth, son las unicas q requieren auth jwt. las de arrica como ser usuario.listar NO requieren el middleware :D
+// y oara hacerlo mas general, se podria poner este use en el archivo api.js de rutas, y elegir directamente q router de los modelos usan el middleware!
+router.use(auth.ensureAuthenticated) 
+router.get('/:idUsuario', usuario.dame); // get con params es mostrar 1 usuario
+router.put('/', usuario.modificar); //put es actualizar
 
 
 module.exports = router;
